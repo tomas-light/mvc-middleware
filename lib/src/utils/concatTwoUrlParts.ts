b41fd44081ac4aea8bc2f23c8ca020a50d1e7aaf
TomasLight <art.ser.ignatiev@gmail.com>
2023-04-07 10:12:29 +0200

    refactored: prepare to version 2.0.0

---
+++ lib/src/utils/concatTwoUrlParts.ts
@@ -0,0 +1,17 @@
+/** concat if has prefix, like ['/api/users', 'profile'] => /api/users/profile */
+export function concatTwoUrlParts(prefix: string, urlPart: string) {
+  const joinedUrl = [prefix, urlPart]
+    .map((str) => (str ? str.trim() : ''))
+    .filter(Boolean)
+    .join('/');
+
+  return removeDoubleSlashes(joinedUrl);
+}
+
+function removeDoubleSlashes(str: string): string {
+  const index = str.indexOf('//');
+  if (index == -1) {
+    return str;
+  }
+  return removeDoubleSlashes(str.replaceAll('//', '/'));
+}
